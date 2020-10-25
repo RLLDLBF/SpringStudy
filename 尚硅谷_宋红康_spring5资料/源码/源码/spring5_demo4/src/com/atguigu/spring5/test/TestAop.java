6cda910c2021bcaf85c5f2e63376895b1b2f9eab
Weizerojust <963369304@qq.com>
2020-10-25 17:48:45 +0800

    add aop test

--- 尚硅谷_宋红康_spring5资料/源码/源码/spring5_demo4/src/com/atguigu/spring5/test/TestAop.java
+++ 尚硅谷_宋红康_spring5资料/源码/源码/spring5_demo4/src/com/atguigu/spring5/test/TestAop.java
@@ -2,8 +2,10 @@ package com.atguigu.spring5.test;
 
 import com.atguigu.spring5.aopanno.User;
 import com.atguigu.spring5.aopxml.Book;
+import com.atguigu.spring5.config.ConfigAop;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestAop {
@@ -20,6 +22,24 @@ public class TestAop {
     public void testAopXml() {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("bean2.xml");
+        Book book = context.getBean("book", Book.class);
+        book.buy();
+    }
+
+    @Test
+    public void testAopAnno2(){
+        //加载配置类
+        ApplicationContext context
+                = new AnnotationConfigApplicationContext(ConfigAop.class);
+        User user = context.getBean("user", User.class);
+        user.add();
+    }
+
+    @Test
+    public void testAopXml2(){
+        //org.springframework.beans.factory.NoSuchBeanDefinitionException: No bean named 'book' available
+        ApplicationContext context
+                = new AnnotationConfigApplicationContext(ConfigAop.class);
         Book book = context.getBean("book", Book.class);
         book.buy();
     }
