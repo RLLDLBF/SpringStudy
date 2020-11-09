c21b6afac508bdac57928060ed311cd94c88ab9f
Weizerojust <963369304@qq.com>
2020-11-09 19:11:50 +0800

    add several url in controller

--- SpringCloud第2季当堂代码2020.2.29/cloud2020/cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/alibaba/controller/PaymentController.java
+++ SpringCloud第2季当堂代码2020.2.29/cloud2020/cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/alibaba/controller/PaymentController.java
@@ -1,15 +1,15 @@
 package com.atguigu.springcloud.alibaba.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @auther zzyy
  * @create 2020-02-23 14:13
  */
 @RestController
+@Slf4j
 public class PaymentController
 {
     @Value("${server.port}")
@@ -18,6 +18,26 @@ public class PaymentController
     @GetMapping(value = "/payment/nacos/{id}")
     public String getPayment(@PathVariable("id") Integer id)
     {
+        log.info("*****返回端口号：");
+
         return "nacos registry, serverPort: "+ serverPort+"\t id"+id;
+    }
+
+    @GetMapping(value = "/payment1/nacos")
+    public String getPayment1()
+    {
+        return "nacos registry";
+    }
+
+    @GetMapping(value = "/payment2/nacos")
+    public String getPayment2()
+    {
+        log.info("*****返回端口号：");
+        return "nacos registry, serverPort: "+ serverPort;
+    }
+
+    @RequestMapping(value="/hello/{id}",method= RequestMethod.GET)
+    public String sayHello(@PathVariable("id") Integer id){
+        return "id:"+id;
     }
 }
